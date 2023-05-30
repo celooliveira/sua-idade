b1faf0154f097f81fbc717ffbfa5768caf22ecbe
Marcelo Martins de Oliveira <mmoliveira@gmail.com>
2023-05-30 08:11:39 -0300

    Implementação de código usando useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useMemo, FormEvent } from 'react'
 import './App.css'
 
+interface ResultadoProps{
+  nome: string;
+  idade: number;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [nome,setNome] = useState("");
+  const [ano, setAno] = useState("");
+  const [resultado, setResultado] = useState<ResultadoProps>()
+
+  function descobrirIdade(e:FormEvent){
+    e.preventDefault();
+    const anoAtual = new Date().getUTCFullYear();
+    setResultado({
+      nome: nome,
+      idade : anoAtual - Number(ano) 
+    })
+
+    setNome("");
+    setAno("");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <h1>Descubra sua idade</h1>
+      <form className='form' onSubmit={descobrirIdade}>
+        <label className='label'>Informe seu nome!</label>
+        <input 
+          className='input'
+          placeholder='Informe deu nome' 
+          value={nome}
+          onChange={(e)=>setNome(e.target.value)}/>
+
+        <label className='label'>Informe o anos que nasceu!</label>
+        <input 
+          className='input' 
+          placeholder='Informe sua idade'
+          value={ano}
+          onChange={(e)=>setAno(e.target.value)}/>
+
+        <button type='submit' onClick={descobrirIdade} >Descobrir a idade</button>
+      </form>
+
+      {resultado && resultado.nome !=='' &&(
+        <section className='result'>
+          <h2>{resultado?.nome}, você tem: <span>{resultado?.idade} anos</span></h2>
+        </section>
+      )}
+
+    </div>  
   )
 }
 
